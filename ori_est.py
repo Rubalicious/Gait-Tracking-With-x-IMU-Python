2b257a191bf4324d792858649ea2e50bebc16195
Joshua "Ruby" Abrams <jabrams@c-path.org>
2021-07-14 15:36:41 -0700

    refactor and cleaned up code

--- ori_est.py
+++ ori_est.py
@@ -13,6 +13,7 @@ from matplotlib import pyplot as plt
 # import pylops
 import pandas as pd
 import os
+# import seaborn as sns
 
 # option = 'IMU' # or 'MARG'
 # parser = argparse.ArgumentParser()
@@ -57,37 +58,49 @@ import os
 # stopTime = 47
 # samplePeriod = 1/256
 
-index = 3
-filePath = './experiment_dataset{}'.format(index)
+# index = 5
+# filePath = './experiment_dataset{}'.format(index)
+# startTime=0
+# stopTime=200
+# samplePeriod=1/256
+
+# index = 6
+filePath = './str8_line_const_vel'
 startTime=0
 stopTime=200
 samplePeriod=1/256
 
 def extract_sensortile_data():
-    dir = "./datasets/Exp{}/".format(index)
+    # dir = "./datasets/Exp{}/".format(index)
+    dir = "./datasets/Str8_line_const_vel/"
+    # dir = "./datasets/Str8_line_varbl_vel/"
     files = os.listdir(dir)
 
     # extract data and skip first three rows
-    acc_data = pd.read_csv(dir+files[0], skiprows=3) # add skiprows=3 for proper file formatting
-    gyr_data = pd.read_csv(dir+files[1], skiprows=3)
-    mag_data = pd.read_csv(dir+files[2], skiprows=3)
+    acc_data = pd.read_csv(dir+files[0], skiprows=4) # add skiprows=3 for proper file formatting
+    gyr_data = pd.read_csv(dir+files[1], skiprows=4)
+    mag_data = pd.read_csv(dir+files[2], skiprows=4)
 
     # extract date information
     # date = acc_data.Date
     # df = pd.Series(date, dtype=np.datetime64)
 
     # compute sample frequency
+    # print(acc_data)
     acc_data['Date']=pd.to_datetime(acc_data['Date'])
+    
     v_sample_rate = acc_data.groupby(acc_data.Date.dt.second).count().Date.median()
     
     # add cumulative lapsed time column
-    acc_data['TimeDiff']=(acc_data.Date-acc_data.Date[0])
-    acc_data['TimeDiff']=acc_data['TimeDiff']/ np.timedelta64(1, 's')
+    # acc_data['TimeDiff']=(acc_data.Date-acc_data.Date[0])
+    # acc_data['TimeDiff']=acc_data['TimeDiff']/ np.timedelta64(1, 's')
     
     # extract only last three columns (X, Y, Z) of data 
     acc_data = acc_data[acc_data.columns[-3:]]
     gyr_data = gyr_data[gyr_data.columns[-3:]]
     mag_data = mag_data[mag_data.columns[-3:]]
+
+    
 
     # convert into correct units
     acc_data = acc_data/1000 # [mg] --> [g]
@@ -104,8 +117,10 @@ def extract_sensortile_data():
 
     data = data[1:]
     data.columns = header
-    data.to_csv("experiment_dataset{}_CalInertialAndMag.csv".format(index))
-    print("dataset expeiment_dataset{}_CalInertialAndMag.csv has been created".format(index))
+    data.to_csv("str8_line_const_vel_CalInertialAndMag.csv")
+    # data.to_csv("str8_line_varbl_vel_CalInertialAndMag.csv")
+    # data.to_csv("experiment_dataset{}_CalInertialAndMag.csv".format(index))
+    # print("dataset str8_line_const_vel_CalInertialAndMag.csv has been created".format(index))
     return v_sample_rate, data
 
 def extract_data(filePath):
@@ -136,13 +151,105 @@ def extract_data(filePath):
     magZ = magZ[indexSel]
     return time, gyrX, gyrY, gyrZ, accX, accY, accZ, magX, magY, magZ  
 
+def plot_raw_data():
+    time, gyrX, gyrY, gyrZ, accX, accY, accZ, magX, magY, magZ = extract_data(filePath)
+    fig = plt.figure(figsize=(12, 7))
+    ax1 = fig.add_subplot(3,1,1)
+    ax2 = fig.add_subplot(3,1,2)
+    ax3 = fig.add_subplot(3,1,3) # 3,1,3
+    ax1.plot(time,gyrX,c='r',linewidth=0.5)
+    ax1.plot(time,gyrY,c='g',linewidth=0.5)
+    ax1.plot(time,gyrZ,c='b',linewidth=0.5)
+    # ax1.plot(time,gyr_mag, c='k',linestyle=":")
+    # ax1.plot(time,100*stationary_gyr, c='k', linewidth=2)
+
+    ax1.set_title("gyroscope")
+    ax1.set_xlabel("time (s)")
+    ax1.set_ylabel("angular velocity (degrees/s)")
+    ax1.legend(["x","y","z"])
+    ax2.plot(time,accX,c='r',linewidth=0.5)
+    ax2.plot(time,accY,c='g',linewidth=0.5)
+    ax2.plot(time,accZ,c='b',linewidth=0.5)
+    # ax2.plot(time,acc_magFilt,c='k',linestyle=":",linewidth=1)
+    # ax2.plot(time,stationary,c='k')
+    ax2.set_title("accelerometer")
+    ax2.set_xlabel("time (s)")
+    ax2.set_ylabel("acceleration (g)")
+    ax2.legend(["x","y","z"])
+
+    ax3.plot(time, magX, c='r', linewidth=0.5)
+    ax3.plot(time, magY,c='g',linewidth=0.5)
+    ax3.plot(time, magZ,c='b',linewidth=0.5)
+    # norm = np.sqrt(magX*magX+magY*magY)
+    # ax3.plot(time, norm,c='k',linewidth=0.5)
+    # ax3.plot(time, mag_mag,c='k', linewidth=0.5, linestyle=':')
+    # ax3.plot(time[:-1], mag_derFilt,c='k', linewidth=0.5, linestyle='--')
+    # ax3.plot(time[:-1], der_threshold,c='k', linewidth=1, linestyle='-')
+    # ax3.plot(time, xinv, linewidth=1, label="pylops")
+
+    ax3.set_title("magnetometer")
+    ax3.set_xlabel("time (s)")
+    ax3.set_ylabel("magnetic field strength (mT)")
+    ax3.legend(["x","y","z"])
+    ax3.legend()
+    plt.show(block=False)
+
+def plot_pos_vel_data(time, pos, vel, sample_frequency):
+    fig = plt.figure(figsize=(10, 7))
+    ax1 = fig.add_subplot(211)
+    ax1.plot(time,vel[:,0],c='r',linewidth=0.5)
+    ax1.plot(time,vel[:,1],c='g',linewidth=0.5)
+    ax1.plot(time,vel[:,2],c='b',linewidth=0.5)
+    ax1.legend(["x","y","z"])
+    ax1.set_title("velocity sampled at {:.2f} Hz".format(sample_frequency))
+    # ax1.set_xlabel("time (s)")
+    ax1.set_ylabel("velocity (m/s)")
+    plt.show(block=False)
+    ax2 = fig.add_subplot(212)
+    ax2.plot(time,pos[:,0],c='r',linewidth=0.5)
+    ax2.plot(time,pos[:,1],c='g',linewidth=0.5)
+    ax2.plot(time,pos[:,2],c='b',linewidth=0.5)
+    ax2.legend(["x","y","z"])
+    ax2.set_title("position sampled at {:.2f} Hz".format(sample_frequency))
+    ax2.set_xlabel("time (s)")
+    ax2.set_ylabel("position (m)")
+    plt.show(block=False)
+
+def plot_trajectory(posPlot, alg, sample_frequency, tau):
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111, projection='3d') # Axe3D object
+
+    ax.plot(posPlot[:,0],posPlot[:,1],posPlot[:,2])
+    # for i in range(0, len(posPlot), 200):
+    #     A = Quaternion(quat[i]).to_DCM()
+    #     ax.quiver(posPlot[i,0],posPlot[i,1],posPlot[i,2], A[:, 0], A[:,1], A[:, 2], color=['r','g','b']) # modify this to quiver.
+    # print(quatPlot[-1])
+    min_, max_ = np.min(np.min(posPlot,axis=0)), np.max(np.max(posPlot,axis=0))
+    ax.set_xlim(min_,max_)
+    ax.set_ylim(min_,max_)
+    ax.set_zlim(min_,max_)
+    ax.set_title('trajectory using {}\nand sample frequency {:.2f}\nthreshold used: {:.4f}'.format(alg, sample_frequency, tau))
+    ax.set_xlabel("x position (m)")
+    ax.set_ylabel("y position (m)")
+    ax.set_zlabel("z position (m)")
+
+    plt.show(block=False)
+    plt.show()
+
 def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_MARG=False, subsample=False):
     # -------------------------------------------------------------------------
     # extract data
-    sample_frequency, _ = extract_sensortile_data()
+    sample_frequency, data = extract_sensortile_data()
+    samplePeriod = 1/sample_frequency
+    # print(data[data.columns[3:6]])
+    # quit()
     # sample_frequency = float(freq)
     time, gyrX, gyrY, gyrZ, accX, accY, accZ, magX, magY, magZ = extract_data(filePath)
 
+    # unit testing
+    # assert((data[data.columns[3]] == accX).all)
+    # assert((data[data.columns[4]] == accY).all)
+    # assert((data[data.columns[5]] == accZ).all)
     # numSamples = len(time)
 
     # factor = scaling # factor by which to subsample data
@@ -177,8 +284,6 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
 
     # -------------------------------------------------------------------------
     # signal processing
-    # take a derivative, filter out high frequency data (not corresponding to walking ) 5-6 Hz
-    # butterworth
 
     # Compute accelerometer magnitude
     acc_mag = np.sqrt(accX*accX+accY*accY+accZ*accZ)
@@ -204,98 +309,20 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
     b, a = signal.butter(1, (2*filtCutOff)/(1/samplePeriod), 'lowpass') #'lowpass'
     acc_magFilt = signal.filtfilt(b, a, acc_magFilt, padtype = 'odd', padlen=3*(max(len(b),len(a))-1))
 
-
-    # Ruby's contribution
-    filtCutOff_mag = 2
-    # bm, am = signal.butter(1, (2*filtCutOff_mag)/(1/samplePeriod), 'lowpass') #'lowpass'
-    # mag_magFilt = signal.filtfilt(bm, am, mag_mag, padtype = 'odd', padlen=3*(max(len(bm),len(am))-1))
-
-    b,a = signal.butter(1, (2*filtCutOff_mag)/(1/samplePeriod), 'lowpass')
-    # magX_Filt = signal.filtfilt(b, a, magX, padtype = 'odd', padlen=3*(max(len(b),len(a))-1))
-
-    # mag_der = np.diff(magX, n=1)
-
-    filtCutOff_mag = 10
-    # bm, am = signal.butter(1, (2*filtCutOff_mag)/(1/samplePeriod), 'lowpass') #'lowpass'
-    # mag_derFilt = signal.filtfilt(bm, am, mag_der, padtype = 'odd', padlen=3*(max(len(bm),len(am))-1))
-
-    # der_threshold = .2*(np.abs(mag_derFilt) > .005)
-
-
-
     # -------------------------------------------------------------------------
-    # denoising magentic field strength data using total variation
-    # magX = restoration.denoise_tv_bregman(magX, weight=.7)
-    # magX = restoration.denoise_tv_chambolle(magX, weight=.5)
-    # Iop = pylops.Identity(N)
-    # y = Iop*magX
-
-    # Dop = pylops.FirstDerivative(N, edge=True, kind='backward')
-    # mu = 0.1     #0.01
-    # lamda = 1   #0.3
-    # niter_out = 50 #50
-    # niter_in = 3
-
-    # xinv, niter = \
-    #     pylops.optimization.sparsity.SplitBregman(Iop, [Dop], y, niter_out,
-    #                                               niter_in, mu=mu, epsRL1s=[lamda],
-    #                                               tol=1e-4, tau=1.,
-    #                                               **dict(iter_lim=30, damp=1e-10))
-
-
     # Threshold detection
     # tau = 0.1
     # tauY = .9
     stationary = acc_magFilt < tau # and gyrY < tauY # originally set to 0.05
 
-    stationary_gyr = gyr_mag < 40
-
-
+    # stationary_gyr = gyr_mag < 40
 
 
     if plot_graphs: 
-        fig = plt.figure(figsize=(12, 7))
-        ax1 = fig.add_subplot(3,1,1)
-        ax2 = fig.add_subplot(3,1,2)
-        ax3 = fig.add_subplot(3,1,3) # 3,1,3
-        ax1.plot(time,gyrX,c='r',linewidth=0.5)
-        ax1.plot(time,gyrY,c='g',linewidth=0.5)
-        ax1.plot(time,gyrZ,c='b',linewidth=0.5)
-        ax1.plot(time,gyr_mag, c='k',linestyle=":")
-        ax1.plot(time,100*stationary_gyr, c='k', linewidth=2)
+        plot_raw_data()
 
-        ax1.set_title("gyroscope")
-        ax1.set_xlabel("time (s)")
-        ax1.set_ylabel("angular velocity (degrees/s)")
-        ax1.legend(["x","y","z"])
-        ax2.plot(time,accX,c='r',linewidth=0.5)
-        ax2.plot(time,accY,c='g',linewidth=0.5)
-        ax2.plot(time,accZ,c='b',linewidth=0.5)
-        ax2.plot(time,acc_magFilt,c='k',linestyle=":",linewidth=1)
-        ax2.plot(time,stationary,c='k')
-        ax2.set_title("accelerometer")
-        ax2.set_xlabel("time (s)")
-        ax2.set_ylabel("acceleration (g)")
-        ax2.legend(["x","y","z"])
 
-        ax3.plot(time, magX, c='r', linewidth=0.5)
-        ax3.plot(time, magY,c='g',linewidth=0.5)
-        ax3.plot(time, magZ,c='b',linewidth=0.5)
-        norm = np.sqrt(magX*magX+magY*magY)
-        ax3.plot(time, norm,c='k',linewidth=0.5)
-        # ax3.plot(time, mag_mag,c='k', linewidth=0.5, linestyle=':')
-        # ax3.plot(time[:-1], mag_derFilt,c='k', linewidth=0.5, linestyle='--')
-        # ax3.plot(time[:-1], der_threshold,c='k', linewidth=1, linestyle='-')
-        # ax3.plot(time, xinv, linewidth=1, label="pylops")
-
-        ax3.set_title("magnetometer with {} samples".format(N))
-        ax3.set_xlabel("time (s)")
-        ax3.set_ylabel("magnetic field strength (mT)")
-        ax3.legend(["x","y","z"])
-        ax3.legend()
-        plt.show(block=False)
-        # plt.show()
-
+    #------------------------------------------------------------------------------------
     # Compute orientation
     quat  = np.zeros((time.size, 4), dtype=np.float64)
 
@@ -310,7 +337,7 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
     mahony = ahrs.filters.Mahony(Kp=1, Ki=0, KpInit=1, Dt=1/sample_frequency)
     madgwick = ahrs.filters.Madgwick(Dt=1/sample_frequency)
     aqua = ahrs.filters.AQUA(Dt=1/sample_frequency)
-    comp = ahrs.filters.Complementary(frequency=1/samplePeriod)
+    comp = ahrs.filters.Complementary(Dt=1/sample_frequency)
     ekf = ahrs.filters.EKF(Dt=1/sample_frequency)
     fourati = ahrs.filters.Fourati(Dt=1/sample_frequency)
 
@@ -369,17 +396,19 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
             if not use_MARG: mag = []
             quat[t,:] = ekf.update(q, gyr=gyr, acc=acc, mag=mag)
 
-
-    # -------------------------------------------------------------------------
+    #------------------------------------------------------------------------------------
     # Compute translational accelerations
 
     # Rotate body accelerations to Earth frame
     acc = []
     for x,y,z,q in zip(accX,accY,accZ,quat):
-        acc.append(q_rot(q_conj(q), np.array([ x, y, z])))
+        acc.append(q_rot(q_conj(q), np.array([ x, y, z]))) 
+        # acc.append()
     acc = np.array(acc)
-    # acc -= np.array([0,0,1])
-    # acc *= 9.81 # converting to units of m/s^2
+    acc -= np.array([0,0,1])
+    acc *= 9.81 # converting to units of m/s^2
+
+    # plot acc vectors in earth frame.
 
     # Compute translational velocities
     # acc[:,2] = acc[:,2] - 9.81
@@ -387,7 +416,7 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
     # acc_offset = np.zeros(3)
     vel = np.zeros(acc.shape)
     for t in range(1,vel.shape[0]):
-        # vel[t,:] = vel[t-1,:] + acc[t,:]/sample_frequency #*samplePeriod
+        # vel[t,:] = vel[t-1,:] + acc[t,:]*samplePeriod
         vel[t,:] = vel[t-1,:] + (acc[t,:]+acc[t-1,:])/(2*sample_frequency) # trapezoisdal rule
         if stationary[t] == True:
             vel[t,:] = np.zeros(3)
@@ -406,68 +435,24 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
     
     # Remove integral drift
     vel = vel - velDrift
-    if plot_graphs: 
-        fig = plt.figure(figsize=(10, 7))
-        ax1 = fig.add_subplot(211)
-        ax1.plot(time,vel[:,0],c='r',linewidth=0.5)
-        ax1.plot(time,vel[:,1],c='g',linewidth=0.5)
-        ax1.plot(time,vel[:,2],c='b',linewidth=0.5)
-        ax1.legend(["x","y","z"])
-        ax1.set_title("velocity sampled at {:.2f} Hz".format(sample_frequency))
-        # ax1.set_xlabel("time (s)")
-        ax1.set_ylabel("velocity (m/s)")
-        plt.show(block=False)
 
     # -------------------------------------------------------------------------
     # Compute translational position
     pos = np.zeros(vel.shape)
     for t in range(1,pos.shape[0]):
-        # pos[t,:] = pos[t-1,:] + vel[t,:]/sample_frequency #*samplePeriod
+        # pos[t,:] = pos[t-1,:] + vel[t,:]*samplePeriod
         pos[t, :] = pos[t-1,:] + (vel[t, :]+vel[t-1,:])/(2*sample_frequency)
 
     # fig = plt.figure(figsize=(10, 5))
     if plot_graphs: 
-        ax2 = fig.add_subplot(212)
-        ax2.plot(time,pos[:,0],c='r',linewidth=0.5)
-        ax2.plot(time,pos[:,1],c='g',linewidth=0.5)
-        ax2.plot(time,pos[:,2],c='b',linewidth=0.5)
-        ax2.legend(["x","y","z"])
-        ax2.set_title("position sampled at {:.2f} Hz".format(sample_frequency))
-        ax2.set_xlabel("time (s)")
-        ax2.set_ylabel("position (m)")
-        plt.show(block=False)
+        plot_pos_vel_data(time, pos, vel, sample_frequency)
 
     # -------------------------------------------------------------------------
     # Plot 3D foot trajectory
 
-    posPlot = pos
-    quatPlot = quat
-
-    extraTime = 20
-    # onesVector = np.ones(int(extraTime*(1/samplePeriod)))
-
     # Create 6 DOF animation
     if plot_graphs: 
-        fig = plt.figure(figsize=(7, 7))
-        ax = fig.add_subplot(111, projection='3d') # Axe3D object
-
-
-        ax.plot(posPlot[:,0],posPlot[:,1],posPlot[:,2])
-        # for i in range(0, len(posPlot), 200):
-        #     A = Quaternion(quat[i]).to_DCM()
-        #     ax.quiver(posPlot[i,0],posPlot[i,1],posPlot[i,2], A[:, 0], A[:,1], A[:, 2], color=['r','g','b']) # modify this to quiver.
-        # print(quatPlot[-1])
-        min_, max_ = np.min(np.min(posPlot,axis=0)), np.max(np.max(posPlot,axis=0))
-        ax.set_xlim(min_,max_)
-        ax.set_ylim(min_,max_)
-        ax.set_zlim(min_,max_)
-        ax.set_title('trajectory using {}\nand sample frequency {:.2f}\nthreshold used: {:.2f}'.format(alg, sample_frequency, tau))
-        ax.set_xlabel("x position (m)")
-        ax.set_ylabel("y position (m)")
-        ax.set_zlabel("z position (m)")
-
-        plt.show(block=False)
-        plt.show()
+        plot_trajectory(pos, alg, sample_frequency, tau)
 
     # -------------------------------------------------------------------------
     # upsample and record data
@@ -482,14 +467,41 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
 
 
 if __name__ == "__main__":
-    pos, quat, vel = build_trajectory(freq=256, tau=0.0, plot_graphs=True, alg="Madgwick", use_MARG=True) # reference data
+    pos, quat, vel = build_trajectory(
+        freq=256,
+        tau=0.005,
+        plot_graphs=True, 
+        alg="Madgwick",
+        use_MARG=True
+    ) # reference data
     # factors = np.linspace(1.0, 3.0, 10)
     # taus = np.linspace(0.05, 0.1, 10)
     # print(factors, taus)
 
-    vel 
+    # vel 
     arclength = 0
     for vx,vy,vz in vel:
         arclength += np.sqrt(vx**2+vy**2+vz**2)
     print("arclength of trajectory = {}".format(arclength))
+
+
+
+    # -------------------------------------------------------------------------
+    # denoising magentic field strength data using total variation
+    # magX = restoration.denoise_tv_bregman(magX, weight=.7)
+    # magX = restoration.denoise_tv_chambolle(magX, weight=.5)
+    # Iop = pylops.Identity(N)
+    # y = Iop*magX
+
+    # Dop = pylops.FirstDerivative(N, edge=True, kind='backward')
+    # mu = 0.1     #0.01
+    # lamda = 1   #0.3
+    # niter_out = 50 #50
+    # niter_in = 3
+
+    # xinv, niter = \
+    #     pylops.optimization.sparsity.SplitBregman(Iop, [Dop], y, niter_out,
+    #                                               niter_in, mu=mu, epsRL1s=[lamda],
+    #                                               tol=1e-4, tau=1.,
+    #                                               **dict(iter_lim=30, damp=1e-10))
 
