df495bf574ea56b02943f19eff6b43073a8fa25f
Joshua "Ruby" Abrams <jabrams@c-path.org>
2021-07-21 10:13:48 -0700

    updating repo

--- ori_est.py
+++ ori_est.py
@@ -70,17 +70,17 @@ import os
 # stopTime=200
 # samplePeriod=1/256
 
-index = 2
-filePath = './v2_track{}'.format(index)
+index = 1
+filePath = './v4_track{}'.format(index)
 startTime=0
-stopTime=200
+stopTime=500
 samplePeriod=1/256
 
 def extract_sensortile_data():
     # dir = "./datasets/Exp{}/".format(index)
     # dir = "./datasets/Str8_line_const_vel/"
     # dir = "./datasets/Str8_line_varbl_vel/"
-    dir = "./datasets/v2_track{}/".format(index)
+    dir = "./datasets/{}/".format(filePath)
     files = os.listdir(dir)
 
     # extract data and skip first three rows
@@ -97,17 +97,16 @@ def extract_sensortile_data():
     acc_data['Date']=pd.to_datetime(acc_data['Date'])
     
     v_sample_rate = acc_data.groupby(acc_data.Date.dt.second).count().Date.median()
-    
-    # add cumulative lapsed time column
-    # acc_data['TimeDiff']=(acc_data.Date-acc_data.Date[0])
-    # acc_data['TimeDiff']=acc_data['TimeDiff']/ np.timedelta64(1, 's')
+    print("sampling rate of data: {} Hz".format(v_sample_rate))
     
     # extract only last three columns (X, Y, Z) of data 
     acc_data = acc_data[acc_data.columns[-3:]]
     gyr_data = gyr_data[gyr_data.columns[-3:]]
     mag_data = mag_data[mag_data.columns[-3:]]
 
-    
+    # add cumulative lapsed time column
+    # acc_data['TimeDiff']=(acc_data.Date-acc_data.Date[0])
+    # acc_data['TimeDiff']=acc_data['TimeDiff']/ np.timedelta64(1, 's')
 
     # convert into correct units
     acc_data = acc_data/1000 # [mg] --> [g]
@@ -124,7 +123,7 @@ def extract_sensortile_data():
 
     data = data[1:]
     data.columns = header
-    data.to_csv("v2_track{}_CalInertialAndMag.csv".format(index))
+    data.to_csv("{}_CalInertialAndMag.csv".format(filePath))
     # data.to_csv("str8_line_const_vel_CalInertialAndMag.csv")
     # data.to_csv("str8_line_varbl_vel_CalInertialAndMag.csv")
     # data.to_csv("experiment_dataset{}_CalInertialAndMag.csv".format(index))
@@ -199,7 +198,6 @@ def plot_raw_data():
     ax3.set_xlabel("time (s)")
     ax3.set_ylabel("magnetic field strength (mT)")
     ax3.legend(["x","y","z"])
-    ax3.legend()
     plt.show(block=False)
 
 def plot_pos_vel_data(time, pos, vel, sample_frequency):
@@ -252,6 +250,25 @@ def compute_arclength(vel):
         arclength += np.sqrt(vx**2+vy**2+vz**2)
     arclength *= samplePeriod
     return arclength
+
+def plot_earth_frame_acc(time, acc, accX, accY, accZ):
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(212)
+    ax.plot(time,acc[:,0],c='r',linewidth=0.5)
+    ax.plot(time,acc[:,1],c='g',linewidth=0.5)
+    ax.plot(time,acc[:,2],c='b',linewidth=0.5)
+    ax.legend(["x","y","z"])
+    ax.set_title("Accelerometer data in Earth's Frame")
+    # ax1.set_xlabel("time (s)")
+    ax.set_ylabel("accelerometer (m/s)")
+    plt.show(block=False)
+    ax2 = fig.add_subplot(211)
+    ax2.plot(time,accX,c='r',linewidth=0.5)
+    ax2.plot(time,accY,c='g',linewidth=0.5)
+    ax2.plot(time,accZ,c='b',linewidth=0.5)
+    ax2.set_title("Accelerometer data in Sensor's Frame")
+    ax2.set_ylabel("accelerometer (g)")
+    ax2.legend(["x","y","z"])
 
 def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_MARG=False, subsample=False):
     global samplePeriod
@@ -380,7 +397,6 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
             q = ekf.update(q, gyr=gyr, acc=acc, mag=mag)
         else:
             raise("Algorithm not defined. Choose between: Madgwick, Mahony, Fourati, and EKF.")
-            quit()
 
     # all data can be returned in this form
     # orientation = Fourati(gyr)
@@ -431,8 +447,8 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
     acc *= 9.81 # converting to units of m/s^2
 
     # plot acc vectors in earth frame.
+    plot_earth_frame_acc(time, acc, accX, accY, accZ)
     
-
     # Compute translational velocities
     # acc[:,2] = acc[:,2] - 9.81
 
@@ -492,10 +508,10 @@ def build_trajectory(freq=256, tau=0.05, alg="Madgwick", plot_graphs=False, use_
 if __name__ == "__main__":
     pos, quat, vel = build_trajectory(
         freq=256,
-        tau=0.01,
+        tau=0.025,
         plot_graphs=True, 
         alg="Madgwick",
-        use_MARG=True
+        use_MARG=False
     ) # reference data
     # factors = np.linspace(1.0, 3.0, 10)
     # taus = np.linspace(0.05, 0.1, 10)
@@ -504,7 +520,7 @@ if __name__ == "__main__":
     # vel 
     
     arclength = compute_arclength(vel)
-    print("arclength of trajectory = {}".format(arclength))
+    print("arclength of trajectory = {:.5f} meters".format(arclength))
 
 
 
